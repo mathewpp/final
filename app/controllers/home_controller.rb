184f9953b5340dd1804d0e1138e61ebf531082da
Mathew Philip <mathewp@uchicago.edu>
2015-08-22 18:36:10 -0500

    added read me

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,10 @@ class HomeController < ApplicationController
 
   def index
   	if session[:user_id]
-  		@user = User.find(session[:user_id])
+  		@user = User.find_by(:id => session[:user_id])
+      if !@user
+        redirect_to '/logout'
+      end  
   	end
 
   	if params["keyword"].present?
