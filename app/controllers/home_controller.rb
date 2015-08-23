2621d6d8f2eec813613c18c34c5d258ecb70b38e
Mathew Philip <mathewp@uchicago.edu>
2015-08-23 00:08:15 -0500

    Final Review 2

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,6 +6,13 @@ class HomeController < ApplicationController
       if !@user
         redirect_to '/logout'
       end  
+      # @followusers = User.includes(:Follow).where("Follows.follower_id != ?", @user.id)
+
+      # @followusers = User.joins("LEFT OUTER JOIN Follows on users.id != Follows.follower_id")
+                       # .where("Follows.follower_id = ?", @user.id)
+      # @followusers = User.where("id NOT IN (SELECT followed_id FROM follows WHERE follower_id = ?", @user.id))
+        @followusers = User.where('id NOT IN (SELECT followed_id FROM follows WHERE follower_id = ' + @user.id.to_s +
+           ')').last(5)
   	end
 
   	if params["keyword"].present?
